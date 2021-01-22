7bac901bc2f936a6a47ba5b358da88a3cbf3da26
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2021-01-22 12:59:27 -0500

    Tous fichiers dans sample, parcours de tous les fichiers

--- ts2famix_TypescriptParser.ts
+++ ts2famix_TypescriptParser.ts
@@ -3,7 +3,7 @@ import { TypescriptParser } from "typescript-parser";
 
 const myAsync = async (): Promise<Record<string, number | string>> => {
     const parser = new TypescriptParser();
-    const parsed = await parser.parseFile('./Car.ts', 'workspace root');
+    const parsed = await parser.parseFile('./sample-ts/Car.ts', 'workspace root');
     console.log(parsed);
     return;
 }
